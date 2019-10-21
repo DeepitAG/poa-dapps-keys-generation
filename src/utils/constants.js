d121d3d4509fae4ffcfa8db9da90a3012ac826a2
Arsenii Petrovich <arseniy.altoros@gmail.com>
2019-10-21 17:14:09 +0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 let constants = {}
 
-constants.organization = 'poanetwork'
+constants.organization = 'DeepitAG'
 constants.repoName = 'poa-chain-spec'
 constants.addressesSourceFile = 'contracts.json'
 constants.ABIsSources = {
@@ -10,24 +10,9 @@ constants.userDeniedTransactionPattern = 'User denied transaction'
 constants.baseURL = '/poa-dapps-keys-generation'
 
 constants.NETWORKS = {
-  '42': {
-    NAME: 'Kovan',
-    BRANCH: 'kovan',
-    TESTNET: true
-  },
-  '77': {
-    NAME: 'Sokol',
-    BRANCH: 'sokol',
-    TESTNET: true
-  },
-  '99': {
-    NAME: 'Core',
-    BRANCH: 'core',
-    TESTNET: false
-  },
-  '100': {
-    NAME: 'Dai',
-    BRANCH: 'dai',
+  '191': {
+    NAME: 'Pchain',
+    BRANCH: 'pchain',
     TESTNET: false
   }
 }
